f0ed55f36a9b19cafdf84ba3f83f51d8361c357f
J4qu3s <jarkko.hoyhtya@gmail.com>
2021-10-26 20:47:02 +0300

    Doing some styling
    Adding css to the project

---
+++ src/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        
+            <div class="ui active inverted dimmer">
+                <div class="ui big text loader">Loading</div>
+            </div>
+            
+        
+    );
+}
+
+export default Loading;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,67 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "semantic-ui-css/semantic.min.css";
 
-/* const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-    return (
-        <div>Latitude : </div>
-    )
-} */
+import SeasonDisplay from "./SeasonDisplay";
+import Loading from "./Loading";
 
 class App extends React.Component {
-    render() {
+
+    /* constructor(props) {
+        //reference to parents (React.Component) constructor class
+        super(props);
+
+        //initialization of state
+        //only place where you can do direct assignment to state
+        this.state = { 
+            lat : null,
+            errorMessage : ""
+            
+        };    
+    } */
+    state = { 
+        lat : null,
+        errorMessage : ""
         
+    };
+
+    componentDidMount() {
+        console.log("Component did mount!");
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+            position => {
+                //setState of react.component is the only way to update state
+                this.setState({lat: position.coords.latitude});
+            },
+            err => {
+                this.setState({errorMessage: err.message});
+                console.log(err)
+            }
         );
-        return (
-            <div>Latitude : </div>
-        )
     }
+
+    componentDidUpdate() {
+        console.log("Component did update!");
+    }
+
+    componentWillUnmount() {
+        console.log("Componen unmounted!")
+    }
+
+    //Mandatory for react to work
+    render() {
+             
+            if(this.state.errorMessage && !this.state.lat){
+                return <div>
+                    Error: {this.state.errorMessage}
+                </div>
+            }
+            if(this.state.lat && !this.state.errorMessage){
+                return <SeasonDisplay lat={this.state.lat}/>
+            }
+            return <Loading />;
+            
+        }      
+    
 }
 
 ReactDom.render(<App/>, document.querySelector('#root'));
